fff3c78002060849aa1f5e4b5c18361ac82ad7db
Bekarys <bemalikov04@gmail.com>
2023-01-31 21:50:26 +0600

    friendsLaptop1

---
+++ For/for1.py
@@ -0,0 +1,6 @@
+for x in "banana":
+  print(x)
+  
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
---
+++ For/for2.py
@@ -0,0 +1,13 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
+print()
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
